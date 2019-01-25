13817bbdc6c7c1507791df68028faa6eda9d2625
justinorjt <justinorjt@gmail.com>
2019-01-24 18:48:33 -0600

    added selection of random posts

--- crawler/ig-crawl.py
+++ crawler/ig-crawl.py
@@ -19,6 +19,7 @@ def gather():
 	basicIG = 'https://www.instagram.com'
 	# tag = '5454jt54'
 	tag = 'lockeddddd'
+	# tag = 'lokkedin'
 
 	tagUrl = theUrl +tag
 	browser = webdriver.Chrome('chromedriver.exe')
@@ -27,28 +28,40 @@ def gather():
 	# get the html and the link
 	browser.get(tagUrl)
 	source = browser.page_source
+	# browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 	pretty = bsoup(source, 'html.parser')
 	body = pretty.find('body')
 	# FIND EVERY POST IN THE BODY
 	post = body.findAll('div',{'class':'_bz0w'})
 	# GET THE LINK TO EVERY POST AND ADD IT TO ARRAY
-	for theLink in post:
+	for theLink in selectRandomPictures(post):
 		href = theLink.find('a').get('href')
 		Link = basicIG+href
 		pictureLinks.append(Link)
 
-	print(pictureLinks)
+	print((pictureLinks))
 
 
 	# CLOSE THE SCRAPING BROWSER
 	browser.quit()
-	
-# def selectRandomPictures():
 
+# something = [1,2,3,4,5,6,7,8,9]
+# other=[]
+# other.extend(range(1, 100))
+def selectRandomPictures(anArray):
+	i =[]
+	i.extend(range(1,9))
+	start = random.choice(i)
+	theGist=[]
+	nex = start
 
+	while nex < len(anArray):
+		theGist.append(anArray[nex])
+		nex += random.choice(i)
 
+	return (theGist)
 
-
+# selectRandomPictures(other)
 
 gather()
 
